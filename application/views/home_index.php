042d0872220447917d78323d29dac194242c4be7
vjroby <vjroby@gmail.com>
2014-08-24 18:51:47 +0300

    home page design

--- application/views/home_index.php
+++ application/views/home_index.php
@@ -12,21 +12,143 @@
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/normalize.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/orbit.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.vertical-tabs.css">
+    <script src="<?php echo base_url(); ?>js/jquery-1.11.1.min.js"></script>
     <script src="<?php echo base_url(); ?>js/vendor/modernizr-2.6.2.min.js"></script>
 </head>
 <body>
+
 <!--[if lt IE 7]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
+<div class="container big-wrapper">
+    <div class="row">
+        <div class="col-md-12 nav-wrapper">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><img src="<?php echo base_url(); ?>/img/logo.jpg" alt=""/></a>
+                    </div>
 
-<!-- Add your site or application content here -->
-<p>Hello world! This is HTML5 Boilerplate.</p>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+        <div class=" carousel-container">
+            <div id="featured">
+             <img  alt=""  src="<?php echo base_url(); ?>img/p7.jpg" />
+                 <a href="http://www.google.com"><img alt="" src="<?php echo base_url(); ?>img/p8.jpg" /></a>
+                 <a href="http://www.google.com"><img  alt="" src="<?php echo base_url(); ?>img/p9.jpg" /></a>
+                 <a href="http://www.google.com"><img  alt="" src="<?php echo base_url(); ?>img/p10.jpg" /></a>
+            </div>
+        </div>
+    </div>
+    <div class="row main-page">
+        <div class="col-md-4">
+            <h3>WHO <span>WE ARE</span></h3>
+
+            <span>
+                Title some title orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </span>
+            <br/>
+            <span>
+               It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+        </div>
+        <div class="col-md-8">
+            <div class="col-md-6"> <!-- required for floating -->
+                <h3>WHAT <span>WE DO</span></h3>
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs tabs-left">
+                    <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
+                    <li><a href="#profile" data-toggle="tab">Profile</a></li>
+                    <li><a href="#messages" data-toggle="tab">Messages</a></li>
+                    <li><a href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-6">
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div class="tab-pane active" id="home"><img src="<?php echo base_url(); ?>/img/marketing_tab.jpg" alt=""/></div>
+                    <div class="tab-pane" id="profile"><img src="<?php echo base_url(); ?>/img/marketing_tab_line.jpg" alt=""/></div>
+                    <div class="tab-pane" id="messages"><img src="<?php echo base_url(); ?>/img/marketing_tab.jpg" alt=""/></div>
+                    <div class="tab-pane" id="settings"><img src="<?php echo base_url(); ?>/img/marketing_tab_line.jpg" alt=""/></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row main-page">
+        <div class="col-md-7">
+            <h3>TESTIMONIALS</h3>
+            <span>
+                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+        </div>
+        <div class="col-md-2">
+            <img src="<?php echo base_url(); ?>/img/social.jpg" alt=""/>
+        </div>
+        <div class="col-md-3 pull-right">
+            <form action="" class="form-horizontal">
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                </div>
+                <textarea class="form-control" rows="3"></textarea>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default">Sign in</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <span>Copyright &copy; 2010 CLR MEDIA. All Rights Reserved</span>
+    </footer>
+</div>
+
 <script src="<?php echo base_url(); ?>js/plugins.js"></script>
+<script src="<?php echo base_url(); ?>js/bootstrap.js"></script>
 <script src="<?php echo base_url(); ?>js/main.js"></script>
+<!--<script src="--><?php //echo base_url(); ?><!--js/infinitecarousel/jquery.infinitecarousel3.min.js"></script>-->
+<script src="<?php echo base_url(); ?>js/jquery.orbit-1.2.3.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#featured').orbit();
+    });
+</script>
 </body>
 </html>
